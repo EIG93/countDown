d517d46d4bfb23e81b51cd4465d5b1783bed308a
EIG93 <whytongxue@outlook.com>
2024-03-02 16:54:37 +0800

    compelete basic features

--- src/components/CountDown.jsx
+++ src/components/CountDown.jsx
@@ -1,14 +1,7 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 import './CountDown.css';
-
-const addPreZero = (number) => {
-  if(number >= 10) {
-    return `${number}`
-  }else {
-    return `0${number}` 
-  }
-}
+import background from '../assets/background.jpg';
 
 export default function CountDown() {
 
@@ -17,27 +10,27 @@ export default function CountDown() {
   const [second, setSecond] = useState(0)
   const [isCounting, setIsCounting] = useState(false)
 
-  const [time, setTime] = useState(20) //default time is 3600 
+  const [time, setTime] = useState(3600 * 5) //default time is 3600 
   useEffect(() => {
     console.log("useEffect--" + isCounting + "---time=" + time);
 
-    if(isCounting) {
-      if(time >= 0) {
+    if (isCounting) {
+      if (time >= 0) {
         const interval = setInterval(() => {
-       
+
           console.log("setInterval触发" + time);
           const calHour = Math.floor(time / 3600);
-          const calMinute =Math.floor((time / 60) % 60);
-          const calSecond =  time % 60;
-      
+          const calMinute = Math.floor((time / 60) % 60);
+          const calSecond = time % 60;
+
           setHour(calHour);
           setMinute(calMinute);
           setSecond(calSecond);
           setTime(time - 1);
         }, 1000)
-  
-        return () =>  clearInterval(interval);
-      }else {
+
+        return () => clearInterval(interval);
+      } else {
         setTime(20)
         setIsCounting(false)
       }
@@ -45,25 +38,34 @@ export default function CountDown() {
 
   }, [isCounting, time])
 
+  const addPreZero = (number) => {
+    if (number >= 10) {
+      return `${number}`
+    } else {
+      return `0${number}`
+    }
+  }
+
+
   function startCountDown() {
-    if(isCounting) {
+    if (isCounting) {
       //to stop
-      if(time > 0) {
+      if (time > 0) {
         setIsCounting(false);
       }
-    }else {
-      if(time <= 0) {
+    } else {
+      if (time <= 0) {
         setTime(20);
       }
-      if(time === 20) {
+      if (time === 20) {
         setTime(time - 1);
       }
-     
+
       setIsCounting(true);
 
     }
     console.log("startCountDown--" + isCounting + "--time = " + time);
-  
+
   }
 
   function reset() {
@@ -81,16 +83,22 @@ export default function CountDown() {
         {/* <div id='year'></div>
         <div id='month'></div>
         <div id='day'></div> */}
-        <div id='hour' className='time'>{addPreZero(hour)}</div>
-        <text className='semicolon'>:</text>
+        
+        <div id='hour' className='time' type='number'>{addPreZero(hour)}</div>
+        <div className='semicolon'>
+          <div >:</div>
+        </div>
+
         <div id='minute' className='time'>{addPreZero(minute)}</div>
-        <label className='semicolon'>:</label>
+        <div className='semicolon'>
+          <c>:</c>
+        </div>
         <div id='second' className='time'>{addPreZero(second)}</div>
       </div>
 
       <div id='btn-container'>
-          <button className='time-btn' onClick={reset}>Reset</button>
-          <button className='time-btn' onClick={startCountDown}>{isCounting?"Stop":"Start"}</button>
+        <button className='time-btn' onClick={reset}>Reset</button>
+        <button className='time-btn' onClick={startCountDown}>{isCounting ? "Stop" : "Start"}</button>
       </div>
     </div>
   )
